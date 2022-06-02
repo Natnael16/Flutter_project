2e8d10fb11592328c770bf23827058f303eb91b1
yadishman <96906829+yadishman@users.noreply.github.com>
2022-06-02 18:06:49 +0300

    Remove unused import

--- gada_ethiopia_mobile/lib/personal info/screens/log-in.dart
+++ gada_ethiopia_mobile/lib/personal info/screens/log-in.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:work/Backup/log-in.dart';
 import 'package:work/bloc/authentication/login/bloc.dart';
 import 'package:work/screens/home.dart';
 
